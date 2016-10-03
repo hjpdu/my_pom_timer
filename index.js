51868cb05f193a8549a6a2ebed71601a7dc745f4
Harryson Pointdujour <Harryson Pointdujour>
2016-10-03 19:24:51 -0400

    changed for heroku deployment

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var server =express();
-var port = 8080;
-
+var port = process.env.PORT || 8080;
 server.use(express.static(__dirname+'/public'));
 
 server.get('/', function(request, response){
